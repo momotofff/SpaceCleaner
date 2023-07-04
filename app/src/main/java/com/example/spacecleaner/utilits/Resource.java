56ec5da80f0a313d39c8c5b51b3a324a3d1cb00b
momotofff <momotofff@mail.ru>
2023-07-04 16:02:28 +0400

    Refactoring metod in Loading class

--- app/src/main/java/com/example/spacecleaner/utilits/Resource.java
+++ app/src/main/java/com/example/spacecleaner/utilits/Resource.java
@@ -8,5 +8,7 @@ public class Resource
     public static Bitmap textureAtlas;
     public static ArrayList<Bitmap> playerSprite = new ArrayList<>();
     public static  ArrayList<Bitmap> playerSpriteUp = new ArrayList<>();
+    public static  ArrayList<Bitmap> playerSpriteDestruction = new ArrayList<>();
+    public static  ArrayList<Bitmap> playerSpriteActionShield = new ArrayList<>();
     public static ArrayList<Bitmap> asteroidSprite = new ArrayList<>();
 }
